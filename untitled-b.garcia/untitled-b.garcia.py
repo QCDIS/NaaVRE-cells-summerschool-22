2e18243482f115d5b9d439547d44e5758bfc1dcd
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-13 16:09:23 +0200

    untitled-b.garcia update

--- untitled-b.garcia/untitled-b.garcia.py
+++ untitled-b.garcia/untitled-b.garcia.py
@@ -3,16 +3,18 @@ arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-arg_parser.add_argument('--processed_data', action='store' , type=int , required='True', dest='processed_data')
+arg_parser.add_argument('--data', action='store' , type=int , required='True', dest='data')
+arg_parser.add_argument('--data2', action='store' , type=int , required='True', dest='data2')
 
 
 args = arg_parser.parse_args()
 
 id = args.id
 
-processed_data = args.processed_data
+data = args.data
+data2 = args.data2
 
 
 
-processed_data
+process_concat= data+data2
 
