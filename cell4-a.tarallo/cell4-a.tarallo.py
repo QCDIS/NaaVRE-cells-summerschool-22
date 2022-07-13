25154efb1bc3361b9d08042980c6feb4f8a0971a
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-13 15:30:54 +0200

    cell4-a.tarallo creation

---
+++ cell4-a.tarallo/cell4-a.tarallo.py
@@ -0,0 +1,18 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+arg_parser.add_argument('--processed_data', action='store' , type=int , required='True', dest='processed_data')
+
+
+args = arg_parser.parse_args()
+
+id = args.id
+
+processed_data = args.processed_data
+
+
+
+print = processed_data
+
