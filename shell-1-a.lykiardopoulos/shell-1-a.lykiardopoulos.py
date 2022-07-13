fd79ea10b320f03a03e733b64bb6f257eb76e1cf
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-13 15:56:03 +0200

    shell-1-a.lykiardopoulos creation

---
+++ shell-1-a.lykiardopoulos/shell-1-a.lykiardopoulos.py
@@ -0,0 +1,14 @@
+
+
+data="my data"
+data2="more data"
+
+import json
+filename = "/tmp/data_" + id + ".json"
+file_data = open(filename, "w")
+file_data.write(json.dumps(data))
+file_data.close()
+filename = "/tmp/data2_" + id + ".json"
+file_data2 = open(filename, "w")
+file_data2.write(json.dumps(data2))
+file_data2.close()
