d62cb9c09828fb2785d2ce6f33ab4175326f0541
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-13 14:35:23 +0200

    test-2-u1-g1 creation

---
+++ test-2-u1-g1/test-2-u1-g1.py
@@ -0,0 +1,19 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+arg_parser.add_argument('--data', action='store' , type=int , required='True', dest='data')
+
+
+args = arg_parser.parse_args()
+
+id = args.id
+
+data = args.data
+
+
+
+
+data_bis = data + 5
+
