1fe396f177d4b56c065aa90b16c7a863082d5f53
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-13 16:34:30 +0200

    untitled-f.sadeghi update

--- untitled-f.sadeghi/untitled-f.sadeghi.py
+++ untitled-f.sadeghi/untitled-f.sadeghi.py
@@ -3,18 +3,17 @@ arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-arg_parser.add_argument('--data_1', action='store' , required='True', dest='data_1')
-arg_parser.add_argument('--data_2', action='store' , required='True', dest='data_2')
+arg_parser.add_argument('--process_data', action='store' , required='True', dest='process_data')
 
 
 args = arg_parser.parse_args()
 
 id = args.id
 
-data_1 = args.data_1
-data_2 = args.data_2
+process_data = args.process_data
 
 
 
-process_data = data_1 + data_2
+process_data.append(3)
+process_data
 
