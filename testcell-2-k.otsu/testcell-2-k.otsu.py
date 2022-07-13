6f6e67719912515e944847d4e29949c769fa197e
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-13 15:59:02 +0200

    testcell-2-k.otsu creation

---
+++ testcell-2-k.otsu/testcell-2-k.otsu.py
@@ -0,0 +1,25 @@
+import argparse
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
+
+arg_parser.add_argument('--data', action='store' , type=int , required='True', dest='data')
+arg_parser.add_argument('--data2', action='store' , type=int , required='True', dest='data2')
+
+
+args = arg_parser.parse_args()
+
+id = args.id
+
+data = args.data
+data2 = args.data2
+
+
+
+processed_data = data + data2
+
+import json
+filename = "/tmp/processed_data_" + id + ".json"
+file_processed_data = open(filename, "w")
+file_processed_data.write(json.dumps(processed_data))
+file_processed_data.close()
