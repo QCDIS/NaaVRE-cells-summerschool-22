8ad9e1651f9682c8683f636a374ec50f1bc59437
skoulouzis <skoulouzis@users.noreply.github.com>
2022-07-14 12:18:21 +0200

    initiate1-t.carval update

--- initiate1-t.carval/initiate1-t.carval.py
+++ initiate1-t.carval/initiate1-t.carval.py
@@ -4,11 +4,11 @@ data0  = 'ola'
 data1  = 'hallo'
 
 import json
-filename = "/tmp/data1_" + id + ".json"
-file_data1 = open(filename, "w")
-file_data1.write(json.dumps(data1))
-file_data1.close()
 filename = "/tmp/data0_" + id + ".json"
 file_data0 = open(filename, "w")
 file_data0.write(json.dumps(data0))
 file_data0.close()
+filename = "/tmp/data1_" + id + ".json"
+file_data1 = open(filename, "w")
+file_data1.write(json.dumps(data1))
+file_data1.close()
