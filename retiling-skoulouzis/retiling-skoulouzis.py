1c6ffa409fa38c610b7e3a71b87227b4301410a7
skoulouzis <skoulouzis@users.noreply.github.com>
2022-10-27 14:45:50 +0200

    retiling-skoulouzis update

--- retiling-skoulouzis/retiling-skoulouzis.py
+++ retiling-skoulouzis/retiling-skoulouzis.py
@@ -1,21 +1,14 @@
-from laserfarm import Retiler
 import pathlib
+from laserfarm import Retiler
+
 import argparse
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
-arg_parser.add_argument('--split_laz_files', action='store' , required='True', dest='split_laz_files')
+arg_parser.add_argument('--split_laz_files', action='store', type=list, required='True', dest='split_laz_files')
 
-arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
-arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
-arg_parser.add_argument('--param_max_x', action='store', type=str, required='True', dest='param_max_x')
-arg_parser.add_argument('--param_max_y', action='store', type=str, required='True', dest='param_max_y')
-arg_parser.add_argument('--param_min_x', action='store', type=str, required='True', dest='param_min_x')
-arg_parser.add_argument('--param_min_y', action='store', type=str, required='True', dest='param_min_y')
-arg_parser.add_argument('--param_n_tiles_side', action='store', type=str, required='True', dest='param_n_tiles_side')
-arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
-arg_parser.add_argument('--param_remote_path_root', action='store', type=str, required='True', dest='param_remote_path_root')
+arg_parser.add_argument('--param_username', action='store', type=str, required='True', dest='param_username')
 
 args = arg_parser.parse_args()
 
@@ -23,33 +16,36 @@ id = args.id
 
 split_laz_files = args.split_laz_files
 
-param_hostname = args.param_hostname
-param_login = args.param_login
-param_max_x = args.param_max_x
-param_max_y = args.param_max_y
-param_min_x = args.param_min_x
-param_min_y = args.param_min_y
-param_n_tiles_side = args.param_n_tiles_side
-param_password = args.param_password
-param_remote_path_root = args.param_remote_path_root
+param_username = args.param_username
 
-conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
-conf_remote_path_split = pathlib.Path(param_remote_path_root + '/split')
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_min_x = '-113107.81'
+conf_n_tiles_side = '512'
+conf_max_y = '726783.87'
+conf_min_y = '214783.87'
+conf_max_x = '398892.19'
+conf_remote_path_split = pathlib.Path(conf_remote_path_root + '/split_'+param_username)
+conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
+conf_remote_path_retiled = pathlib.Path(conf_remote_path_root + '/retiled_'+param_username)
 conf_local_tmp = pathlib.Path('/tmp')
 
-conf_remote_path_retiled = pathlib.Path(param_remote_path_root + '/retiled/')
-conf_remote_path_split = pathlib.Path(param_remote_path_root + '/split')
-conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
+conf_min_x = '-113107.81'
+conf_n_tiles_side = '512'
+conf_max_y = '726783.87'
+conf_min_y = '214783.87'
+conf_max_x = '398892.19'
+conf_remote_path_split = pathlib.Path(conf_remote_path_root + '/split_'+param_username)
+conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
+conf_remote_path_retiled = pathlib.Path(conf_remote_path_root + '/retiled_'+param_username)
 conf_local_tmp = pathlib.Path('/tmp')
+split_laz_files
 remote_path_retiled = str(conf_remote_path_retiled)
 
 grid_retile = {
-    'min_x': float(param_min_x),
-    'max_x': float(param_max_x),
-    'min_y': float(param_min_y),
-    'max_y': float(param_max_y),
-    'n_tiles_side': int(param_n_tiles_side)
+    'min_x': float(conf_min_x),
+    'max_x': float(conf_max_x),
+    'min_y': float(conf_min_y),
+    'max_y': float(conf_max_y),
+    'n_tiles_side': int(conf_n_tiles_side)
 }
 
 retiling_input = {
@@ -62,10 +58,9 @@ retiling_input = {
     'cleanlocalfs': {}
 }
 
-    
-file = split_laz_files
-retiler = Retiler(file.replace('"',''),label=file).config(retiling_input).setup_webdav_client(conf_wd_opts)
-retiler_output = retiler.run()
+for file in split_laz_files:
+    retiler = Retiler(file.replace('"',''),label=file).config(retiling_input).setup_webdav_client(conf_wd_opts)
+    retiler_output = retiler.run()
 
 import json
 filename = "/tmp/retiler_output_" + id + ".json"
