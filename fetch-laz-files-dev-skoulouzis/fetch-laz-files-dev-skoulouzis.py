d90e1f8d89cc6d157aabf268b4719ee69eb7672c
skoulouzis <skoulouzis@users.noreply.github.com>
2022-10-28 13:54:52 +0200

    fetch-laz-files-dev-skoulouzis update

--- fetch-laz-files-dev-skoulouzis/fetch-laz-files-dev-skoulouzis.py
+++ fetch-laz-files-dev-skoulouzis/fetch-laz-files-dev-skoulouzis.py
@@ -15,11 +15,11 @@ id = args.id
 
 
 
-conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
 conf_remote_path_ahn = conf_remote_path_root
+conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
 
-conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
 conf_remote_path_ahn = conf_remote_path_root
+conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
 print(conf_remote_path_ahn)
 laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
