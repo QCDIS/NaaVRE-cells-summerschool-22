22a2dd880142cf6839e39e18cc3cca44ad075272
skoulouzis <skoulouzis@users.noreply.github.com>
2022-11-23 01:53:53 +0100

    split-big-files-dev-skoulouzis update

--- split-big-files-dev-skoulouzis/split-big-files-dev-skoulouzis.py
+++ split-big-files-dev-skoulouzis/split-big-files-dev-skoulouzis.py
@@ -1,8 +1,8 @@
+import os
+import laspy
 import numpy as np
 import pathlib
 from webdav3.client import Client
-import laspy
-import os
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -17,21 +17,22 @@ args = arg_parser.parse_args()
 
 id = args.id
 
-laz_files = args.laz_files
+import json
+laz_files = json.loads(args.laz_files)
 
 param_username = args.param_username
 
-conf_remote_path_ahn =  '/webdav/LAZ'
-conf_laz_compression_factor = '7'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
-conf_max_filesize = '262144000'  # desired max file size (in bytes)
 conf_wd_opts = { 'webdav_hostname':  'https://lfw-ds001-i022.lifewatch.dev:32443/', 'webdav_login':  '20BNXDdL8mg24OaD', 'webdav_password':  'zDoy0hNKkcnsdsQ@OYAVd'}
+conf_max_filesize = '262144000'  # desired max file size (in bytes)
+conf_laz_compression_factor = '7'
+conf_remote_path_ahn =  '/webdav/LAZ'
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
 
-conf_remote_path_ahn =  '/webdav/LAZ'
-conf_laz_compression_factor = '7'
-conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
-conf_max_filesize = '262144000'  # desired max file size (in bytes)
 conf_wd_opts = { 'webdav_hostname':  'https://lfw-ds001-i022.lifewatch.dev:32443/', 'webdav_login':  '20BNXDdL8mg24OaD', 'webdav_password':  'zDoy0hNKkcnsdsQ@OYAVd'}
+conf_max_filesize = '262144000'  # desired max file size (in bytes)
+conf_laz_compression_factor = '7'
+conf_remote_path_ahn =  '/webdav/LAZ'
+conf_remote_path_split = pathlib.Path( '/webdav/LAZ' + '/split_'+param_username)
 
 
 def save_chunk_to_laz_file(in_filename, 
